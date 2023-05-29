55561553f5c3a9a69675ae07f1e4337da43f588f
Tom Daniel <tomudoh258@gmail.com>
2023-05-29 12:04:49 +0100

    Setup React APP

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/Home';
+import Details from './components/Details';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Detail />
+     </div>
+  );
+}
+
+export default App;
