acb9775195102e5fcd37988154337f652884617d
Tom Daniel <tomudoh258@gmail.com>
2023-05-30 22:16:10 +0100

    Add styles to  homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData, selectData, selectStatus } from '../redux/Home/homeSlice';
 import { Link } from 'react-router-dom';
+import bgGridImage from '../assets/Bg-grid.jpeg';
+import bgMainImage from '../assets/Bg-main.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   const dispatch = useDispatch();
   const data = useSelector(selectData);
   const status = useSelector(selectStatus);
   const [searchTerm, setSearchTerm] = useState('');
+  const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
     dispatch(fetchData());
@@ -17,11 +23,17 @@ const Home = () => {
     setSearchTerm(e.target.value);
   };
 
-  const filteredCountries = data.rawData ? data.rawData.filter((country) => {
-    const searchTermLower = searchTerm.toLowerCase();
-    const countryRegionLower = country.Country_Region.toLowerCase();
-    return countryRegionLower.includes(searchTermLower);
-  }) : [];
+  const handleIconClick = () => {
+    setIsExpanded(true);
+  };
+
+  const filteredCountries = data.rawData
+    ? data.rawData.filter((country) => {
+        const searchTermLower = searchTerm.toLowerCase();
+        const countryRegionLower = country.Country_Region.toLowerCase();
+        return countryRegionLower.includes(searchTermLower);
+      })
+    : [];
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -33,25 +45,52 @@ const Home = () => {
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search country..."
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <div className='container'>
-      {filteredCountries.length > 0 ? (
-        filteredCountries.map((country) => (
-          <div key={country.Combined_Key} className='list'>
-            <ul>
-              <Link to={`/details/${country.Combined_Key}`}>{country.Admin2} {country.Country_Region}</Link>
-            <li>Total: {country.Confirmed}</li>
-            </ul>
-          </div>
-        ))
-      ) : (
-        <div>No matching countries found</div>
-      )}
+      <nav>
+        <h1>COVID 19 METRIC DATA</h1>
+
+        <FontAwesomeIcon icon={faMicrophone} size="lg" color="white" className='svg' />
+        <div className="search-container">
+          {!isExpanded && (
+            <FontAwesomeIcon
+              icon={faSearch}
+              size="lg"
+              color="white"
+              className="search-icon"
+              onClick={handleIconClick}
+            />
+          )}
+          {isExpanded && (
+            <input
+              type="text"
+              placeholder="Search country..."
+              value={searchTerm}
+              onChange={handleSearch}
+              className="search-input"
+            />
+          )}
+        </div>
+      </nav>
+      <img src={bgGridImage} alt="BG-grid" className="img" />
+      <div className='stats'>STAT BY COUNTRY</div>
+      <div className="container">
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <div
+              key={country.Combined_Key}
+              style={{ backgroundImage: `url(${bgMainImage})` }}
+              className="list"
+            >
+              <ul>
+                <Link to={`/details/${country.Combined_Key}`}>
+                  {country.Admin2} {country.Country_Region}
+                </Link>
+                <li>Cases: {country.Confirmed}</li>
+              </ul>
+            </div>
+          ))
+        ) : (
+          <div>No matching countries found</div>
+        )}
       </div>
     </div>
   );
